cd420e783d93f70b318954a50c1d270ffc80cd52
iloiacono <ignazio.loiacono@gmail.com>
2021-05-04 08:19:14 +0200

    Edit update.sh

--- update.sh
+++ update.sh
@@ -7,9 +7,9 @@ sail top > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     ./vendor/bin/sail up -d
 fi
-./vendor/bin/sail artisan migrate:fresh --seed
 ./vendor/bin/sail artisan livewire:discover
 ./vendor/bin/sail artisan view:clear
 ./vendor/bin/sail artisan cache:clear
 ./vendor/bin/sail artisan vendor:publish --tag=filament-config
+./vendor/bin/sail artisan migrate:fresh --seed
 ./vendor/bin/sail artisan prestashop:update-all
